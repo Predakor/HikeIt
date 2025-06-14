eba406dd40b223143b2876e73cdc40d86a3dfa82
Predakor <patrykbusko@gmail.com>
2025-06-14 13:47:48 +0200

    Add: Config for the gpx data processing

---
+++ Domain/TripAnalytics/Configs/ElevationProfileConfig.cs
@@ -0,0 +1,9 @@
+﻿namespace Domain.TripAnalytics.Configs;
+
+public record ElevationProfileConfig(
+    float? MaxElevationSpike = 10f,
+    float? EmaSmoothingAlpha = .4f,
+    float? MedianFilterWindowSize = 5,
+    int? RoundingDecimalsCount = 1,
+    int? DownsamplingFactor = 10
+);
