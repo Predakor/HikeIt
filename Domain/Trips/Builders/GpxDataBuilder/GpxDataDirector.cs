695cf5ba6295d94cca09394c0145ba6ebd59cd9c
Predakor <patrykbusko@gmail.com>
2025-06-20 00:15:52 +0200

    Change: moved gpxproccesing config into its own class

--- Domain/Trips/Builders/GpxDataBuilder/GpxDataDirector.cs
+++ Domain/Trips/Builders/GpxDataBuilder/GpxDataDirector.cs
@@ -1,24 +1,17 @@
-﻿using Domain.TripAnalytics.Configs;
+﻿using Domain.Trips.Config;
 using Domain.Trips.ValueObjects;
 
 namespace Domain.Trips.Builders.GpxDataBuilder;
 
-public record PointsToPreserve(List<GpxPoint> Maximas, List<GpxPoint> Minimas);
+public record PointsToPreserve(IEnumerable<GpxPoint> Maximas, IEnumerable<GpxPoint> Minimas);
 
 public record ElevationProfileData(AnalyticData Data, PointsToPreserve? Points = null);
 
-public record ElevationDataWithConfig(ElevationProfileData Data, ElevationProfileConfig Config);
-
-record DefaultConfig(
-    float MaxElevationSpike = 10f,
-    float EmaSmoothingAlpha = 0.88f,
-    int MedianFilterWindowSize = 7,
-    int RoundingDecimalsCount = 1
-) : IConfig<GpxDataBuilder>;
+public record ElevationDataWithConfig(ElevationProfileData Data);
 
 internal static class GpxDataDirector {
     public static AnalyticData AnalyticData(List<GpxPoint> points) {
-        var config = new DefaultConfig();
+        var config = GpxDataConfigs.GpxFile;
         return new GpxDataBuilder(points)
             .ClampElevationSpikes(config.MaxElevationSpike)
             .ApplyMedianFilter(config.MedianFilterWindowSize)
@@ -28,12 +21,12 @@ internal static class GpxDataDirector {
     }
 
     public static AnalyticData ElevationProfile(ElevationProfileData data) {
-        var config = new DefaultConfig();
+        var config = GpxDataConfigs.ElevationProfile;
 
-        return new GpxDataBuilder(data.Data.Points).DownSample(10).Build();
+        return new GpxDataBuilder(data.Data.Points).DownSample(config.DownsamplingFactor).Build();
     }
 
-    public static AnalyticData FromConfig(ElevationProfileData data, ElevationProfileConfig config) {
+    public static AnalyticData FromConfig(ElevationProfileData data, ConfigBase.Nullable config) {
         var builder = new GpxDataBuilder(data.Data.Points);
         var (
             MaxElevationSpike,
@@ -42,8 +35,6 @@ internal static class GpxDataDirector {
             RoundingDecimalsCount,
             DownsamplingFactor
         ) = config;
-        Console.WriteLine("im here and i see ");
-        Console.WriteLine(DownsamplingFactor);
 
         if (MaxElevationSpike != null) {
             builder.ClampElevationSpikes((double)MaxElevationSpike);
---
+++ Domain/Trips/Config/GpxDataConfigs.cs
@@ -0,0 +1,32 @@
+﻿namespace Domain.Trips.Config;
+
+public abstract record ConfigBase() {
+    public record GpxFile(
+        float MaxElevationSpike = 10f,
+        float EmaSmoothingAlpha = 0.88f,
+        int MedianFilterWindowSize = 7,
+        int RoundingDecimalsCount = 1
+    );
+
+    public record ElevationProfile(
+        float MaxElevationSpike = 10f,
+        float EmaSmoothingAlpha = 0.88f,
+        int MedianFilterWindowSize = 7,
+        int RoundingDecimalsCount = 1,
+        int DownsamplingFactor = 10
+    );
+
+    public record Nullable(
+        float? MaxElevationSpike = null,
+        float? EmaSmoothingAlpha = null,
+        int? MedianFilterWindowSize = null,
+        int? RoundingDecimalsCount = null,
+        int? DownsamplingFactor = null
+    );
+}
+
+public static class GpxDataConfigs {
+    public static ConfigBase.GpxFile GpxFile => new();
+    public static ConfigBase.ElevationProfile ElevationProfile => new();
+    public static ConfigBase.Nullable Nullable => new();
+}
