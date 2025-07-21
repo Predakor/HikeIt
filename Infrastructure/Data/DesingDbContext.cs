a190ea930af6f3f43085c42f8345542226a15395
Predakor <patrykbusko@gmail.com>
2025-07-21 16:06:37 +0200

    Fix: add design time db context for postgress

--- Infrastructure/Data/DesingDbContext.cs
+++ Infrastructure/Data/DesingDbContext.cs
@@ -6,10 +6,21 @@ namespace Infrastructure.Data;
 public class TripDbContextFactory : IDesignTimeDbContextFactory<TripDbContext> {
     public TripDbContext CreateDbContext(string[] args) {
         var optionsBuilder = new DbContextOptionsBuilder<TripDbContext>();
-        string connectionString =
-            "Server=localhost\\sqlexpress;Database=hikeit;Trusted_Connection=True;TrustServerCertificate=True;";
-        // Provide your connection string here (or load from config/env)
-        optionsBuilder.UseSqlServer(connectionString, x => x.UseNetTopologySuite());
+
+        // Read an environment variable to decide which DB to use
+        var environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "Production";
+
+        string connectionString;
+        if (environment == "Development") {
+            connectionString = "Server=localhost\\sqlexpress;Database=hikeit;Trusted_Connection=True;TrustServerCertificate=True;";
+            optionsBuilder.UseSqlServer(connectionString, x => x.UseNetTopologySuite());
+        }
+        else {
+            connectionString = Environment.GetEnvironmentVariable("ConnectionStrings__DefaultConnection")
+                ?? throw new InvalidOperationException("Connection string not found.");
+
+            optionsBuilder.UseNpgsql(connectionString, x => x.UseNetTopologySuite());
+        }
 
         return new TripDbContext(optionsBuilder.Options);
     }
