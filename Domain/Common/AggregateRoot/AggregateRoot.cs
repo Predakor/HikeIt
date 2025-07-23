2e5bd812263c12fd392c6af2d1f182adc561a0c3
Predakor <patrykbusko@gmail.com>
2025-07-23 23:05:01 +0200

    Add: Base classes for domain event and root aggregate

---
+++ Domain/Common/AggregateRoot/AggregateRoot.cs
@@ -0,0 +1,17 @@
+﻿using Domain.Interfaces;
+
+namespace Domain.Common.AggregateRoot;
+
+internal class AggregateRoot<TId> : IEntity<TId> {
+    public required TId Id { get; init; }
+
+    readonly List<IDomainEvent> _events = [];
+
+    public IReadOnlyCollection<IDomainEvent> Events => _events.AsReadOnly();
+
+    protected void AddDomainEvent(IDomainEvent domainEvent) => _events.Add(domainEvent);
+
+    protected void RemoveDomainEvent(IDomainEvent domainEvent) => _events.Remove(domainEvent);
+
+    public void ClearDomainEvents() => _events.Clear();
+}
