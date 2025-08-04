22e4cedffe535bed61498e0357e4740f35e7f5ac
Predakor <patrykbusko@gmail.com>
2025-08-04 14:15:41 +0200

    Add: Local storage persistance

--- hikeit.client/src/main.tsx
+++ hikeit.client/src/main.tsx
@@ -1,23 +1,33 @@
 import { Provider } from "@/components/ui/provider";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { QueryClient } from "@tanstack/react-query";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router";
 import Layout from "./Layout/Layout";
 import RenderRoutes from "./components/RenderRoutes/RenderRoutes";
+
+import { createAsyncStoragePersister } from "@tanstack/query-async-storage-persister";
+import { PersistQueryClientProvider } from "@tanstack/react-query-persist-client";
 import "./index.css";
 
 const queryClient = new QueryClient();
+
+const asyncStoragePersister = createAsyncStoragePersister({
+  storage: window.localStorage,
+});
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
       <Provider>
-        <QueryClientProvider client={queryClient}>
+        <PersistQueryClientProvider
+          client={queryClient}
+          persistOptions={{ persister: asyncStoragePersister }}
+        >
           <Layout>
             <RenderRoutes />
           </Layout>
-        </QueryClientProvider>
+        </PersistQueryClientProvider>
       </Provider>
     </BrowserRouter>
   </StrictMode>
