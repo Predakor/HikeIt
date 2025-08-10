b477db8b2eb0edbfd4b37ccad5dce8d3e3ddb776
Predakor <patrykbusko@gmail.com>
2025-08-10 22:59:03 +0200

    Add: Role support on the backend and basic admin user

---
+++ Infrastructure/Data/Seeding/Users/InsertBaseUser.cs
@@ -0,0 +1,27 @@
+﻿using Domain.Users;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Infrastructure.Data.Seeding.Users;
+
+internal class InsertBaseUser : ISeeder {
+    readonly UserManager<User> _userManager;
+    readonly User _user;
+
+    public InsertBaseUser(IServiceProvider provider) {
+        _userManager = provider.GetRequiredService<UserManager<User>>();
+        _user = User.DemoUser;
+    }
+
+    public async Task Seed(TripDbContext dbContext) {
+        if (_user is null) {
+            throw new Exception("default user can't be null");
+        }
+
+        var hasNoDemoUser = (await _userManager.FindByNameAsync(_user.UserName!)) == null;
+        if (hasNoDemoUser) {
+            Console.WriteLine("Seeding default users");
+            await _userManager.CreateAsync(_user, "Default123!");
+        }
+    }
+}
---
+++ Infrastructure/Data/Seeding/Users/InsertRoles.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Infrastructure.Data.Seeding.Users;
+internal class InsertRoles : ISeeder {
+    public static readonly string[] roles = ["Admin", "Moderator", "User", "Demo"];
+
+    readonly RoleManager<IdentityRole<Guid>> _roleManager;
+
+    public InsertRoles(IServiceProvider provider) {
+        _roleManager = provider.GetRequiredService<RoleManager<IdentityRole<Guid>>>();
+
+    }
+
+    public async Task Seed(TripDbContext dbContext) {
+        foreach (var role in roles) {
+            if (!await _roleManager.RoleExistsAsync(role)) {
+                Console.WriteLine($"Seeding Role: {role}");
+                await _roleManager.CreateAsync(new IdentityRole<Guid>(role));
+            }
+        }
+    }
+}
