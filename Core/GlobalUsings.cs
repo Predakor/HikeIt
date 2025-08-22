842a3c8a73a7859f4e070c213e2994bdd1b78dd3
Predakor <patrykbusko@gmail.com>
2025-08-22 20:53:04 +0200

    fix

--- Core/GlobalUsings.cs
+++ Core/GlobalUsings.cs
@@ -1,15 +1,4 @@
 ﻿global using Core.Extentions;
 global using Core.Results;
-global using Core.Validators;
 
 namespace Core;
-
-class Blocker {
-    AbstractValidator<object> validator;
-
-    string[] x = [" c", "d "];
-
-    void xd() {
-        if (x.NotNullOrEmpty()) { }
-    }
-}
