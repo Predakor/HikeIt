ae0d1ded9a40647d4e3a544fd3cfccc84147454c
Predakor <patrykbusko@gmail.com>
2026-03-08 13:29:48 +0100

    Remove base path and move site to its own domain

--- Frontend/vite.config.ts
+++ Frontend/vite.config.ts
@@ -1,16 +1,12 @@
-import { defineConfig, loadEnv } from "vite";
 import plugin from "@vitejs/plugin-react";
-import tsconfigPaths from "vite-tsconfig-paths";
+import { defineConfig } from "vite";
 import { analyzer } from "vite-bundle-analyzer";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), "");
-  return {
-    base: env.VITE_BASE_PATH || "/",
-    plugins: [plugin(), tsconfigPaths(), analyzer()],
-    server: {
-      port: 54840,
-    },
-  };
+export default defineConfig({
+  plugins: [plugin(), tsconfigPaths(), analyzer()],
+  server: {
+    port: 54840,
+  },
 });
