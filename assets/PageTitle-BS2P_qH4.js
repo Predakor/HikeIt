bf0d99a3ddc52b398d86282663a74437674d3bbc
Predakor <Predakor@users.noreply.github.com>
2026-03-08 12:30:37 +0000

    deploy: ae0d1ded9a40647d4e3a544fd3cfccc84147454c

--- assets/PageTitle-BS2P_qH4.js
+++
@@ -1,1 +0,0 @@
-import{j as t}from"./index-_7WMkc_7.js";import{aq as i}from"./App-CMWyngnU.js";function s({title:e}){return t.jsx(i,{size:{base:"2xl",lg:"4xl"},fontWeight:"bold",children:e})}export{s as P};
--- assets/UserSettingsPage-B_QdMFOP.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./index-_7WMkc_7.js";function r(){return e.jsx("div",{children:"UserSettingsPage"})}export{r as default};
